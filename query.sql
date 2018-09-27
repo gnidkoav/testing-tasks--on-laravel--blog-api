a85c8854d509cc4e1040e93a269de7a99e6db7dc
Alexey V. Gnidko <gnidkoav@email.fake>
2018-09-27 22:05:53 +0300

    SQL query file was deleted

--- query.sql
+++
@@ -1,1 +0,0 @@
-SELECT `id` as `ArticleID`, `title` as `ArticleTitle`, (SELECT count(*) FROM `comments` WHERE `comments`.`article_id` = `articles`.`id`) as `NumberOfComments` FROM `articles` WHERE (SELECT count(*) FROM `comments` WHERE `comments`.`article_id` = `articles`.`id`) > 100;
